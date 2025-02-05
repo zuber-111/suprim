7b092a8a2293a6a3ea887753c25433cfed9f3bee
mod <mod@gmail.com>
2025-02-05 13:25:15 +0530

    adding file to repo

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,7 @@
+  GNU nano 8.2                                                                  Helloworld.java
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello world!");
     }
 }
+
